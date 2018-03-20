abf063933d150c85f894fea4b5d4c68d81cbac61
ronalkinho <dquinones@ucm.es>
2018-03-20 12:26:17 +0100

    ClienteServidor en JAVA
    Clases: conexion, servidor, cliente, mainCliente,mainServidor

---
+++ JAVA/TFG/src/MainCliente.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+//Clase principal que hará uso del cliente
+public class MainCliente
+{
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+}
+    
---
+++ JAVA/TFG/src/MainServidor.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+
+//Clase principal que hará uso del servidor
+public class MainServidor
+{
+    public static void main(String[] args) throws IOException
+    {
+        Servidor serv = new Servidor(); //Se crea el servidor
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+}
