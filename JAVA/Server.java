5703adf8a64995f98677d2f83396ceccb195e993
ronalkinho <dquinones@ucm.es>
2018-05-23 10:44:31 +0200

    Añadidas las 3 clases
    Añadida encriptación de archivos en el envío de ellos

--- JAVA/Server.java
+++ JAVA/Server.java
@@ -19,18 +19,26 @@ import java.io.FileWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.security.Key;
 import java.util.Scanner;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 class Server{
 
 	public static void main(String args[])throws Exception{  
 
 		//Se declara un string y pide un nombre de archivo para leerlo
+		Cipher cipher = Cipher.getInstance("AES");
+		String myKey = "qwertyuiopasdfgh";
+		Key secretKey = new SecretKeySpec(myKey.getBytes(), "AES");
+		cipher.init(Cipher.DECRYPT_MODE, secretKey);
+		
 		String fileName;
-
-		while(true){
-			//Crea un server socket en un puerto fijo, en este caso el 5000
-			ServerSocket ss=new ServerSocket(5000); 
+		//Crea un server socket en un puerto fijo, en este caso el 5000
+		ServerSocket ss=new ServerSocket(5000); 
+		while(!ss.isClosed()){
 			System.out.println ("Waiting for request");
 			Socket s=ss.accept();  
 			System.out.println ("Connected With "+s.getInetAddress().toString());
@@ -40,6 +48,10 @@ class Server{
 			try{
 
 				fileName = din.readUTF();
+				if(fileName.equals("Stop")) {
+					ss.close();
+					throw new Exception();
+				}
 				System.out.println("Receiving File: "+fileName);
 				//Lo manda a la carpeta por defecto files
 				File file = new File(fileName);
@@ -51,28 +63,29 @@ class Server{
 				String line;
 				int count;
 				byte[] buffer = new byte[8192];
+				byte[] bufferDeciph = new byte[8192];
 				char[] buffc = new char[8192];
-				
+
 				while((count = din.read(buffer)) > 0){
-					bw.write(buffer);
+					bufferDeciph = cipher.update(buffer, 0, buffer.length);
+					bw.write(bufferDeciph);
 				}
+				System.out.println("Closing file");
 				bw.close();
 				
 			}
-
-
 			catch(Exception e)
 			{
 			
 			}
-			//Siempre se jeecuta da igual la rama por la que vayas
+			//Siempre se ejecuta da igual la rama por la que vayas
 			finally{
-				bw.close();
+				if(bw != null)
+					bw.close();
 				din.close();
 				dout.close();
-				ss.close();
-
 			}
 		}
+		System.out.println("Stopping server");
 	}
 }  
--- JAVA/Window.java
+++ JAVA/Window.java
@@ -12,7 +12,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -29,10 +37,22 @@ public class Window extends JFrame {
 	private JTextField filePathTextField;
 	private JButton selectButton;
 	private JButton sendButton;
+	private JButton stopButton;
 	private final JFileChooser fc = new JFileChooser();
 	private File selectFile;
+	private Cipher cipher;
 	
 	public Window() {
+		try {
+			cipher=Cipher.getInstance("AES");
+			String myKey = "qwertyuiopasdfgh";
+			SecretKey key = new SecretKeySpec(myKey.getBytes(), "AES");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		this.selectFile = null;
 		serverIpLabel = new JLabel("Server Address: ");
 		serverIpTextField = new JTextField(40);
@@ -40,17 +60,30 @@ public class Window extends JFrame {
 		filePathTextField = new JTextField(40);
 		selectButton = new JButton("Select");
 		sendButton = new JButton("Send");
+		stopButton = new JButton("Stop Server");
 		this.setLayout(new BorderLayout());
 		JPanel panelNorth = new JPanel(new FlowLayout());
 		panelNorth.add(serverIpLabel);
 		panelNorth.add(serverIpTextField);
 		JPanel panelCenter = new JPanel(new FlowLayout());
+		JPanel panelSouth = new JPanel(new FlowLayout());
 		panelCenter.add(filePathTextField);
 		panelCenter.add(selectButton);
+		panelSouth.add(sendButton);
+		panelSouth.add(stopButton);
 		this.add(panelNorth, BorderLayout.NORTH);
 		this.add(panelCenter, BorderLayout.CENTER);
-		this.add(sendButton, BorderLayout.SOUTH);
+		this.add(panelSouth, BorderLayout.SOUTH);
 		this.setTitle("SEND FILES");
+		
+		stopButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stopAction();
+				
+			}
+		});
 		
 		selectButton.addActionListener(new ActionListener() {
 			
@@ -62,7 +95,7 @@ public class Window extends JFrame {
 					filePathTextField.setText(selectFile.getAbsolutePath());
 				}
 				else {
-					//No hacer nada
+					//TODO cifrar el archivo cuando lo vaya a mandar para que el servidor lo 
 				}
 				
 			}
@@ -104,13 +137,14 @@ public class Window extends JFrame {
 				dout.writeLong(size);
 				int count;
 				byte[] buffer = new byte[8192];
+				byte[] bufferCiph = new byte[8192];
 				char[] buffc = new char[8192];
 				while((count = br.read(buffc)) > 0) {
 					for(int i=0; i < count; ++i) {
 						buffer[i] = (byte) buffc[i];	
 					}
-	
-					dout.write(buffer);
+					bufferCiph = cipher.update(buffer, 0, buffer.length);
+					dout.write(bufferCiph);
 				}
 				JOptionPane.showMessageDialog(this, "File successfully sent","File sent", JOptionPane.INFORMATION_MESSAGE);
 				dout.close();
@@ -119,13 +153,41 @@ public class Window extends JFrame {
 				s.close();
 
 			} catch (IOException e) {
-				JOptionPane.showMessageDialog(this, e.getMessage(),"Error", JOptionPane.ERROR);
+				JOptionPane.showMessageDialog(this, e.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
 			}
 
 			
 		}
 			
 		
+	}
+	
+	private void stopAction() {
+		
+		String serverAddress = serverIpTextField.getText();
+		if((serverAddress == null)||(serverAddress.isEmpty())){
+			JOptionPane.showMessageDialog(this, "You must define a server address","Warning", JOptionPane.WARNING_MESSAGE);
+
+		}
+		else {
+			try {
+				Socket s=new Socket(serverAddress,5000);
+				DataInputStream din = new DataInputStream(s.getInputStream());  
+				DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
+				String line;
+				dout.writeUTF("Stop");
+				JOptionPane.showMessageDialog(this, "Server stopped","Server stopped", JOptionPane.INFORMATION_MESSAGE);
+				dout.close();
+				din.close();
+				s.close();
+
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(this, e.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
+			}
+
+			
+		}
+			
 	}
 	
 	
